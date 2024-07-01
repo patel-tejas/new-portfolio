7dea5c646b14e6e462760ccbbe1bc2009db67e9d
Tejas Patel <pateltejas.2005@gmail.com>
2024-07-01 19:52:55 +0530

    made ui changes in assistant

--- components/Chatbot.jsx
+++ components/Chatbot.jsx
@@ -1,20 +1,21 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 const {
     GoogleGenerativeAI,
 } = require("@google/generative-ai");
 import { IoChatboxOutline, IoClose } from "react-icons/io5";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { FaRobot } from "react-icons/fa";
+import chatBot from "../public/bot1.jpg";
 import Image from 'next/image';
-import robot from "../public/195.jpg";
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 // console.log(apiKey);
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction: "You are an AI Assistant for my portfolio website. Your name is Tez. You need to answer questions based on me. I am Tejas Patel a full stack web developer based on Ahmedabad. The tech stack I use is Nextjs based on MERN stack. (Mongodb, Express, React and Nodejs). I also use Prisma ORM to work with multiple databases while using only single code system for all of them. My portfolio website has 4 pages (Home, Contact, Projects, and About Me). I have done Diploma in Information Technology and currently pursuing Bachelors. Along with that, I know Java and Python along with Javascript. I am doing DSA (Data structures and Algorithm in java). Have solved more than 100 problems on Leetcode. If anyone asks about contacting me give my gmail id: techtezoffficial@gmail.com and give a nice message to hit me up. Well along with the skill side I love reading books and binge series and movies for Entertainment. My favorite music genre is pop. tell user to explore the music player (Spotify music embed) which I have attached at the end of every page in the site to explore my taste. Well you can add your slight taste to every message but it should be user friendly and sarcastic sometimes. Every respond should be short like 2-3 sentences (some can be long depend on the question). Well if someone ask question outside of my arena or anything that doesn't revolve around me and things I mentioned then give a user friendly message that you cannot reply to such questions cause my developer (ofc me) hasn't trained you to do that. If anyone asks about projects which I have done tell him that I have done full-fledged projects containing all CRUD operations and relations. Have built a SaaS AI software using OpenAI. Built a Blog app containing every functionality. Have made Event app where user can create events, join events with tickets and stripe integrations.\n",
+    systemInstruction: "You are an AI Assistant for my portfolio website. Your name is Tez. You need to answer questions based on me. I am Tejas Patel a full stack web developer based on Ahmedabad. The tech stack I use is Nextjs based on MERN stack. (Mongodb, Express, React and Nodejs). I also use Prisma ORM to work with multiple databases while using only single code system for all of them. My portfolio website has 4 pages (Home, Contact, Projects, and About Me). I have done Diploma in Information Technology and currently pursuing Bachelors. Along with that, I know Java and Python along with Javascript. I am doing DSA (Data structures and Algorithm in java). Have solved more than 100 problems on Leetcode. If anyone asks about contacting me give my gmail id: techtezoffficial@gmail.com and give a nice message to hit me up. Well along with the skill side I love reading books and binge series and movies for Entertainment. My favorite music genre is pop. tell user to explore the music player (Spotify music embed) which I have attached at the end of every page in the site to explore my taste. Well you can add your slight taste to every message but it should be user friendly and sarcastic sometimes. Every respond should be short like 2-3 sentences (some can be long depend on the question). Well if someone ask question outside of my arena or anything that doesn't revolve around me and things I mentioned then give a user friendly message that you cannot reply to such questions cause my developer (ofc me) hasn't trained you to do that. If anyone asks about projects which I have done tell him that I have done full-fledged projects containing all CRUD operations and relations. Have built a SaaS AI software using OpenAI. Built a Blog app containing every functionality. Have made Event app where user can create events, join events with tickets and stripe integrations. Well for other skills i also know Firebase, Prisma, Redux, Recoil, Zustand and more.\n",
 });
 
 const generationConfig = {
@@ -26,15 +27,13 @@ const generationConfig = {
 };
 
 export default function Chatbot() {
-
     const [input, setInput] = useState("");
     const [history, setHistory] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const [loading, setLoading] = useState(false);
+    const chatContainerRef = useRef(null);
 
     const handleSend = async () => {
-        // console.log("Input: ", input);
-
         try {
             setInput("");
             setLoading(true);
@@ -44,8 +43,6 @@ export default function Chatbot() {
             });
             const result = await chatSession.sendMessage(input);
             const responseText = await result.response.text();
-            // console.log("Response: ", responseText);
-            // console.log("History: ", history);
             const user_input = {
                 role: "user",
                 parts: [{ text: input }]
@@ -55,52 +52,59 @@ export default function Chatbot() {
                 parts: [{ text: responseText }]
             };
 
-            setHistory(prevHistory => {
-                // console.log("History: ", prevHistory);
-                return [...prevHistory];
-            });
-
-            
+            setHistory(prevHistory => [...prevHistory]);
             setLoading(false);
-            // console.log("History: ", history);
         } catch (error) {
             console.error("Error: ", error);
             setLoading(false);
         }
     }
 
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [history, loading]);
 
     return (
         <>
             {isOpen && (
                 <>
                     <div className='fixed inset-0 bg-black bg-opacity-50 z-40' onClick={() => setIsOpen(false)}></div>
-                    <div className='fixed bottom-[15%] left-[60%] xs:left-[0vw] xs:w-[100vw] md:left-[30vw] md:w-[60vw] w-[30vw] h-[70vh] bg-white px-3 py-3 flex flex-col justify-between rounded-lg text-sm z-50 animate-slide-in    ai-chat'>
-                        <div className='overflow-scroll flex flex-col flex-1 stroke-secondary overflow-x-hidden overflow-y-visible gap-4'>
+                    <div className='fixed bottom-[15%] left-[60%] xs:left-[5vw] xs:w-[90vw] md:left-[30vw] md:w-[60vw] w-[30vw] h-[70vh] bg-white px-3 py-3 flex flex-col justify-between rounded-lg text-sm z-50 animate-slide-in'>
+                        <div className='overflow-scroll flex flex-col flex-1 overflow-x-hidden overflow-y-visible gap-4' ref={chatContainerRef}>
                             <div className='flex flex-col items-center'>
-                                <h3 className='text-center text-xl font-bold text-blue-500'> <span className='text-orange-500'>Tez</span> here !</h3>
-                                <h3 className='text-center text-xl font-bold text-blue-500'> Tejas's Personal Chatbot 🥷</h3>
+                                <Image src={chatBot} alt="chatbot" width={100} height={100} className='w-[100px] h-[100px]' />
                             </div>
+                            <p className='text-left flex gap-1'>
+                                <FaRobot className='text-3xl px-1 py-1 bg-blue-400 rounded-full text-white ' />
+                                <span className='flex-1 mx-2 bg-gray-200 px-3 py-1 rounded-lg'>Hello! I'm Tez, your friendly AI guide. A virtual version of Tejas 😅. I can answer your questions about his skills, projects, and interests. Feel free to ask away!</span>
+                            </p>
                             {history.map((chat, index) => (
                                 <div key={index}>
                                     {chat.role === "user" && (
                                         <div className='flex justify-end w-full'>
-                                            <p className='text-right inline-block mx-2 bg-blue-400 text-white px-3 py-1 rounded-lg'>{chat.parts[0].text}</p>
+                                            <p className='text-right inline-block mx-2 bg-orange-400 text-white px-3 py-1 rounded-lg'>{chat.parts[0].text}</p>
                                         </div>
                                     )}
-                                    {chat.role === "model" && (
-                                        <p className='text-left flex gap-1'>
-                                            <FaRobot className='text-3xl px-1 py-1 bg-orange-400 rounded-full text-white ' />
-                                            <span className='flex-1 mx-2 bg-gray-200 px-3 py-1 rounded-lg'>{chat.parts[0].text}</span>
-                                        </p>
-                                    )}
+                                    {(chat.role === "model") &&
+                                        (
+                                            <p className='text-left flex gap-1'>
+                                                <FaRobot className='text-3xl px-1 py-1 bg-blue-400 rounded-full text-white ' />
+                                                <span className='flex-1 mx-2 bg-gray-200 px-3 py-1 rounded-lg'>{chat.parts[0].text}</span>
+                                            </p>
+                                        )
+                                    }
                                 </div>
                             ))}
-                            {
-                                loading && (
-                                    <h3 className='flex items-center justify-center'>Loading...</h3>
-                                )
-                            }
+                            {loading && (
+                                <p className='text-left flex gap-1 items-center'>
+                                    <FaRobot className='text-3xl px-1 py-1 bg-blue-400 rounded-full text-white mr-3' />
+                                    <div class='h-2 w-2 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+                                    <div class='h-2 w-2 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+                                    <div class='h-2 w-2 bg-black rounded-full animate-bounce'></div>
+                                </p>
+                            )}
                         </div>
                         <div className='flex gap-1 mt-2'>
                             <input
@@ -114,13 +118,14 @@ export default function Chatbot() {
                         </div>
                     </div>
                 </>
-            )}
+            )
+            }
 
-            <div className='w-[60px] fixed bottom-[5%] left-[90%] xs:left-[75vw] md:left-[89vw] h-[60px] bg-blue-500 flex items-center justify-center rounded-full cursor-pointer group hover:bg-blue-600 duration-200 hover:scale-110 hover:shadow-lg group' onClick={() => setIsOpen(!isOpen)}>
+            <div className='w-[60px] fixed bottom-[5%] left-[90%] xs:left-[75vw] md:left-[89vw] h-[60px] bg-blue-500 flex items-center justify-center rounded-full cursor-pointer group hover:bg-blue-600 duration-200 hover:scale-110 hover:shadow-lg group z-50' onClick={() => setIsOpen(!isOpen)}>
                 <p className='w-[10vw] absolute hidden xs:group-hover:hidden group-hover:inline-block text-center left-[-160px] text-black'>AI assistant to help you 👋</p>
-                <button >
+                <button>
                     {
-                        isOpen ? <IoClose className='text-3xl text-white' /> : <IoChatboxOutline className='text-3xl text-white' />
+                        isOpen ? <MdOutlineKeyboardArrowDown className='text-3xl text-white ' /> : <IoChatboxOutline className='text-3xl text-white' />
                     }
                 </button>
             </div>
